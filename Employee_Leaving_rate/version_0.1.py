d62d46cb10fae6696b55875e45ce677714509be1
nathdip <nathdip@gmail.com>
2017-11-07 09:34:56 +0100

    Folders and everything

---
+++ Employee_Leaving_rate/version_0.1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 00:51:58 2017
+
+@author: dipankar_nath
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
